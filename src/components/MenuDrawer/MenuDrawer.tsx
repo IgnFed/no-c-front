24d9863b9413afe1f5fe829a055d8d5d5f4d7cc3
IgnFed <ignaciofedorenco2317@gmail.com>
2022-10-19 06:01:17 -0300

    Fixed errors

--- src/components/MenuDrawer/MenuDrawer.tsx
+++ src/components/MenuDrawer/MenuDrawer.tsx
@@ -1,20 +1,20 @@
-import React from "react";
+import React, { ReactHTMLElement } from "react";
 import {
-  Drawer,
-  DrawerBody,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerOverlay,
-  DrawerContent,
-  DrawerCloseButton,
-  useDisclosure,
-  Button,
-  Input,
-  Image,
-  Text,
-  Box,
-  List,
-  Link as ChakraLink,
+	Drawer,
+	DrawerBody,
+	DrawerFooter,
+	DrawerHeader,
+	DrawerOverlay,
+	DrawerContent,
+	DrawerCloseButton,
+	useDisclosure,
+	Button,
+	Input,
+	Image,
+	Text,
+	Box,
+	List,
+	Link as ChakraLink,
 } from "@chakra-ui/react";
 import menu from "../../assets/img/menu.png";
 import perfil from "../../assets/img/perfil.png";
@@ -22,83 +22,79 @@ import PhotoPerfilSmall from "../../icons/PhotoPerfilSmall";
 import { Link as RouterLink } from "react-router-dom";
 
 function MenuDrawer() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const btnRef = React.useRef();
+	const { isOpen, onOpen, onClose } = useDisclosure();
+	const btnRef = React.useRef<any>();
 
-  const menuDrawItems = [
-    "Perfil",
-    "Mis tours",
-    
-  ];
+	const menuDrawItems = ["Perfil", "Mis tours"];
 
-  return (
-    <>
-      <Image
-        src={menu}
-        ref={btnRef}
-        onClick={onOpen}
-        w="70px"
-        padding={3}
-        cursor={"pointer"}
-      />
+	return (
+		<>
+			<Image
+				src={menu}
+				ref={btnRef}
+				onClick={onOpen}
+				w="70px"
+				padding={3}
+				cursor={"pointer"}
+			/>
 
-      <Drawer
-        isOpen={isOpen}
-        placement="right"
-        onClose={onClose}
-        finalFocusRef={btnRef}
-      >
-        <DrawerOverlay />
-        <DrawerContent>
-          <DrawerCloseButton />
-          <DrawerHeader
-            display={"flex"}
-            justifyContent="start"
-            alignItems="center"
-            fontWeight={"medium"}
-          >
-            <Image src={perfil} w="25px" marginRight={1} />
-            <Text marginLeft={2}>JHOMAR</Text>
-          </DrawerHeader>
+			<Drawer
+				isOpen={isOpen}
+				placement="right"
+				onClose={onClose}
+				finalFocusRef={btnRef}
+			>
+				<DrawerOverlay />
+				<DrawerContent>
+					<DrawerCloseButton />
+					<DrawerHeader
+						display={"flex"}
+						justifyContent="start"
+						alignItems="center"
+						fontWeight={"medium"}
+					>
+						<Image src={perfil} w="25px" marginRight={1} />
+						<Text marginLeft={2}>JHOMAR</Text>
+					</DrawerHeader>
 
-          <DrawerBody marginX={-1}>
-            <List>
-              {menuDrawItems.map((item, index) => (
-                <ChakraLink
-                  as={RouterLink}
-                  to={item === "Perfil" ? "profile" : item}
-                  key={index}
-                  onClick={onClose}
-                >
-                  <Box
-                    display={"flex"}
-                    justifyContent="space-between"
-                    fontWeight={"medium"}
-                    fontSize={21}
-                    marginTop={2}
-                  >
-                    <Text>{item}</Text>
+					<DrawerBody marginX={-1}>
+						<List>
+							{menuDrawItems.map((item, index) => (
+								<ChakraLink
+									as={RouterLink}
+									to={item === "Perfil" ? "profile" : item}
+									key={index}
+									onClick={onClose}
+								>
+									<Box
+										display={"flex"}
+										justifyContent="space-between"
+										fontWeight={"medium"}
+										fontSize={21}
+										marginTop={2}
+									>
+										<Text>{item}</Text>
 
-                    {item === "Perfil" && <PhotoPerfilSmall />}
-                  </Box>
-                </ChakraLink>
-              ))}
-            </List>
-          </DrawerBody>
+										{item === "Perfil" && <PhotoPerfilSmall />}
+									</Box>
+								</ChakraLink>
+							))}
+						</List>
+					</DrawerBody>
 
-          <DrawerFooter display={""}>
-            <List>
-              <ChakraLink onClick={onClose}>
-                <Box fontWeight={"medium"} fontSize={21} marginTop={2}>
-                  <Text>Cerrar sesión</Text>
-                </Box>
-              </ChakraLink>
-            </List>
-          </DrawerFooter>
-        </DrawerContent>
-      </Drawer>
-    </>
-  );
+					<DrawerFooter display={""}>
+						<List>
+							<ChakraLink onClick={onClose}>
+								<Box fontWeight={"medium"} fontSize={21} marginTop={2}>
+									<Text>Cerrar sesión</Text>
+								</Box>
+							</ChakraLink>
+						</List>
+					</DrawerFooter>
+				</DrawerContent>
+			</Drawer>
+		</>
+	);
 }
 
 export default MenuDrawer;
